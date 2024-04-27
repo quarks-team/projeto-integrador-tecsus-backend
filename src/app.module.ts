0db91ff5cda52b6c096ee83a0cfd4340ce6a43a2
lucassbarcelos <lucasbarcelos58@gmail.com>
2024-04-26 22:08:06 -0300

    refactor: 💡 ajusting some lint issues and removing unused files
    BREAKING CHANGE: 🧨 N

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommonModule } from './infra/common.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Billing } from './domain/entity/billing.entity';
 import { TypeormService } from './infra/typeorm/typerorm.service';
 import { TypeormModule } from './infra/typeorm/typeorm.module';
 import { ApiModule } from './api/api.module';
@@ -15,7 +14,6 @@ import { ApiModule } from './api/api.module';
       useClass: TypeormService,
     }),
     TypeormModule,
-    TypeOrmModule.forFeature([Billing]),
     ApiModule,
   ],
   controllers: [AppController],
