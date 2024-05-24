54225d2311c8ebd3ca035285124490a4efbb93dd
Leonardo Adler da Silva <88751032+LeoAdlerr@users.noreply.github.com>
2024-05-24 01:10:58 -0300

    chore: tests for trigger

--- src/api/billing.controller.ts
+++ src/api/billing.controller.ts
@@ -1,17 +1,21 @@
 import {
   Controller,
+  Get,
   Post,
   UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { BillingService } from 'src/domain/service/billing.service';
+import { GenerateWatterFact } from 'src/domain/use-case/generate-watter-fact.use-case';
 import * as path from 'path';
 import { mkdir, writeFile } from 'fs/promises';
 
 @Controller('billing')
 export class BillingController {
-  constructor(private readonly service: BillingService) {}
+  constructor(private readonly service: BillingService,
+    private readonly fatoService: GenerateWatterFact
+  ) {}
 
   @Post('upload')
   @UseInterceptors(FilesInterceptor('files'))
@@ -25,5 +29,10 @@ export class BillingController {
       await writeFile(filePath, file.buffer);
       await this.service.transform(file.originalname, filePath);
     }
+  }
+
+  @Get('fato')
+  async fato() {
+    this.fatoService.execute();
   }
 }
