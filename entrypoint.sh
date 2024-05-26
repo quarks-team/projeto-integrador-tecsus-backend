d1c93d42c6752a0749069cb6e832f7204c986108
Felipe Sobral <felipesobral_@hotmail.com>
2024-05-26 01:04:30 -0300

    hotfix: automate container configurations at Dockerfiles

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Start MySQL service in the background
+docker-entrypoint.sh mysqld &
+
+# Wait for MySQL to start
+while ! mysqladmin ping -h"localhost" --silent; do
+    sleep 1
+done
+
+# Run initial setup commands without requiring a password
+mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "SET GLOBAL log_bin_trust_function_creators = 1;"
+
+# Keep the container running with an interactive bash shell
+tail -f /dev/null
