be2ec1762540057c4c0fc4fde33bd52dd7efbd9b
Felipe Sobral <felipesobral_@hotmail.com>
2024-05-26 02:12:48 -0300

    hotfix: fixing tests and build

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
